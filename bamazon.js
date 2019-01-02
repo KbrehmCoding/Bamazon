b713ca1cd3d629628ba5520a6ee7b78ed60bd3a5
Krista Brehm <kristabrehm1@gmail.com>
2019-01-01 22:39:53 -0500

    completed fucntions to purchase items, display cost, and update stock info

--- bamazon.js
+++ bamazon.js
@@ -1,9 +1,11 @@
-var mysql = require("mysql");
-var inquirer = require("inquirer");
+const mysql = require("mysql");
+const inquirer = require("inquirer");
 
 require("dotenv").config();
 
-var connection = mysql.createConnection({
+var products;
+
+const connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: process.env.MYSQL_USER,
@@ -11,13 +13,65 @@ var connection = mysql.createConnection({
     database: "bamazon_db"
 });
 
+function init() {
+    readProducts();
+}
+
 function readProducts() {
     console.log("Complete Stock Tally");
     connection.query("SELECT * FROM products", (err, results) => {
         if (err) throw err;
-        console.log(results);
-        connection.end();
+        products = results;
+        results.forEach(product => {
+            console.log(`ID: ${product.item_id} - ${product.product_name} - $${product.price}`);
+        });
+        promptUser();
     });
 }
 
-readProducts();
+function promptUser() {
+    inquirer.prompt([
+        {
+            name: "id",
+            type: "input",
+            message: "Please enter the ID of the product you want to buy:"
+        },
+        {
+            name: "quantity",
+            type: "input",
+            message: "How many would you like to buy?"
+        }
+    ]).then(answers => {
+        if (!checkQuantityOfItem(answers.id, answers.quantity)) {
+            console.log("Insufficient quantity!");
+            connection.end();
+            return;
+        }
+        console.log("Purchase was successful!");
+        const cost = calculateCostOfPurchase(answers.id, answers.quantity);
+        console.log(`That will cost $${cost}`);
+        adjustQuantityOfItem(answers.id, answers.quantity);
+    });
+}
+
+function getProductById(itemId) {
+    return products.find(product => product.item_id === parseInt(itemId));
+}
+
+function checkQuantityOfItem(itemId, quantity) {
+    return getProductById(itemId).stock_quantity >= parseInt(quantity);
+}
+
+function calculateCostOfPurchase(itemId, quantity) {
+    return parseInt(getProductById(itemId).price) * quantity;
+}
+
+function adjustQuantityOfItem(itemId, quantity) {
+    const sql = "UPDATE products SET stock_quantity = stock_quantity - ? WHERE item_id = ?";
+    connection.query(sql, [ parseInt(quantity), itemId ], (err, result) => {
+        connection.end();
+        if (err) throw err;
+    });
+}
+
+init();
