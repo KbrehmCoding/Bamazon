62c458d0c97fea85c9a455d701863c5bdafde6a2
Krista Brehm <kristabrehm1@gmail.com>
2019-01-01 21:21:09 -0500

    create connection to db using environment variables

--- bamazon.js
+++ bamazon.js
@@ -1,3 +1,23 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 
+require("dotenv").config();
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: "bamazon_db"
+});
+
+function readProducts() {
+    console.log("Complete Stock Tally");
+    connection.query("SELECT * FROM products", (err, results) => {
+        if (err) throw err;
+        console.log(results);
+        connection.end();
+    });
+}
+
+readProducts();
